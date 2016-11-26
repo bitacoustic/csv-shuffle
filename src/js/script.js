4c4ea5ac520544168c66969873e7332859b4a011
Eric C. Black <eric@bitacoustic.net>
2016-11-26 10:03:57 -0800

    Further style refinements

--- src/js/script.js
+++ src/js/script.js
@@ -3,35 +3,39 @@ var pos = 0;
 var reachedEnd = false;
 
 $(document).ready(function() {
-  $('#button-file-load').click( function () {
-    console.log("Clicked load button");
-    if ( ! window.FileReader ) {
+  
+  /* change file input listener */
+
+  $('#file-input').on('change', function () {
+    console.log("Changed file input");
+    if (! window.FileReader) {
       return alert( 'FileReader API is not supported by your browser.' );
     }
     var $i = $('#file-input'),
     input = $i[0];
-    if ( input.files && input.files[0] ) {
+    
+    if (input.files && input.files[0]) {
       file = input.files[0]; // The file
       fr = new FileReader(); // FileReader instance
       fr.readAsText(file);
       fr.onload = function () {
         cards = $.csv.toArrays(fr.result);
         console.log(cards);
-        $('#section-card-display').css('display','initial');
+        $('[id^=section-button-display]').css('display','initial');
         $('#card-output').prepend("<p class='output-info'>Loaded deck '" + escape(file.name) + "' in default order</p>");
       };
-
     } else {
       alert( "No file to read. If you've chosen a file, your browser may not be supported." )
     }
-  }); // end $( '#file-load' ).click( function () { }
+  }); 
+
+  /* handle button clicks */
 
   $('#button-shuffle').click( function () {
     console.log("Clicked shuffle button");
     cards = shuffle(cards);
     console.log(cards);
-    $('#button-draw').css('display','initial');
-    $('#button-draw').css('visibility','inherit');
+    $('#button-draw').css('display','initial').css('visibility','inherit');
     $('#card-output').prepend("<p class='output-info'>Shuffled deck</p>");
   });
 
@@ -53,10 +57,12 @@ $(document).ready(function() {
   $('#button-draw').click( function () {
     console.log("Clicked draw button");
     checkReachedEnd();
+
     if (!reachedEnd) {
       card = drawCard();
       console.log(card);
       $('#card-output').prepend("<p id='card-at-pos-" + pos + "' class='output-card'>" + card + "</p>");
+      
       if (pos > 0) {
         previousCard = '#card-at-pos-' + (pos-1);
         $(previousCard).addClass('output-card-visited');
@@ -75,12 +81,10 @@ $(document).ready(function() {
 
   $('#button-create-log').click( function () {
     console.log("Clicked create log button");
-    // newWindow = window.open();
     newWindow = window.open('', 'csv-shuffle log', 'width = 500, height = 500')
     newWindowContent = $('#card-output').html();
     $(newWindow.document.body).html(newWindowContent);
   });  
-
 }) // end document ready
 
 /* helpers */
